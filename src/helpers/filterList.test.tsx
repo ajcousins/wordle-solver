9500d0bbb9058f43e6d7f87a9e1ddff07056674a
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-01-29 22:36:49 +0000

    Yellow test

--- src/helpers/filterList.test.tsx
+++ src/helpers/filterList.test.tsx
@@ -8,7 +8,17 @@ const queryChars1 = [
   { char: "D", status: 1 },
   { char: "E", status: 1 },
 ];
-
 test("ABODE all grey", () => {
   expect(filterList(libraryArr, queryChars1)).toEqual(["smith", "wrung"]);
+});
+
+const queryChars2 = [
+  { char: "B", status: 2 },
+  { char: "A", status: 2 },
+  { char: "E", status: 2 },
+  { char: "O", status: 2 },
+  { char: "D", status: 2 },
+];
+test("ABODE one yellow", () => {
+  expect(filterList(libraryArr, queryChars2)).toEqual(["abode"]);
 });
