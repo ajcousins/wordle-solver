9e20e73c5887211be8bc9b6c889c4bb7956636a1
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-02-04 23:28:44 +0000

    Working scripts

---
+++ server/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// app.use(express.json());
+
+app.use(express.static("../client/build"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
