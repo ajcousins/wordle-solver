1fa98adc9ffa9cda07baef2e6a93e5014720c271
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-02-13 17:20:09 +0000

    Confirmed positions parse

--- client/src/helpers/refineList.ts
+++ client/src/helpers/refineList.ts
@@ -4,5 +4,32 @@ export const refineList = (wordList:string[], knowledge:Knowledge) => {
 
     const newWordList = [...wordList];
 
+    // Parse Knowledge
+    const alphabet = Object.keys(knowledge); // a, b, c, d...
+
+    const confirmedPositions = new Array(5).fill(undefined);
+    for (let i = 0; i < confirmedPositions.length; i++) {
+        for (let j = 0; j < alphabet.length; j++) {
+            if (knowledge[alphabet[j]].confirmedPos.indexOf(i) !== -1) {
+                confirmedPositions.splice(i, 1, alphabet[j]);
+            }
+        }
+    }
+
+    console.log("confirmedPositions:", confirmedPositions);
+    
+
+
+    newWordList.filter((word) => {
+
+        // 1. word MUST have letter in any confirmed positions
+    
+        // 2. word MUST have letter if presence is confirmed in knowledge
+    
+        // 3. each letter must be possible in its position
+
+
+    })
+
     return newWordList;
 }
