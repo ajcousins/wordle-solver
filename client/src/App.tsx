92c6a71130941fffa93c405a3011c7ac1b1d0222
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-07-09 10:59:38 +0100

    Prep for gh-pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
-import Canvas from './components/Canvas'
+import Canvas from './components/Canvas';
+
+function getWindowHeight() {
+  return window.innerHeight;
+}
 
 function App() {
+  const [windowHeight, setWindowHeight] = useState<number>(getWindowHeight());
+  // Dynamic height. vh unit in CSS is broken for full screen apps.
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowHeight(getWindowHeight());
+    }
+    window.addEventListener('resize', handleWindowResize);
+    window.addEventListener('orientationchange', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+      window.removeEventListener('orientationchange', handleWindowResize);
+    };
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" style={{ height: windowHeight }}>
       <Canvas />
     </div>
   );
